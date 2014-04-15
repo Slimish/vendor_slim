8bb5fd3b89cbcd638221cd75cda1b72fda9935af
Tyler Irwin <tyler.s.irwin@gmail.com>
2014-04-14 19:44:04 -0500

    fixup build for skyrocket..

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,4 @@ add_lunch_combo lego_d2lte-userdebug
 add_lunch_combo lego_falcon-userdebug
 add_lunch_combo lego_hercules-userdebug
 add_lunch_combo lego_mako-userdebug
-add_lunch_combo lego-skyrocket-userdebug
+add_lunch_combo lego_skyrocket-userdebug
